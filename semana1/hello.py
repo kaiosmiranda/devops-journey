fc2f9f779c57179a89357bc12096536ffc47f6a1
Kaio Miranda <kaiodbel@outlook.com>
2025-10-10 03:08:00 -0300

    Created the gretting function

--- semana1/hello.py
+++ semana1/hello.py
@@ -1,9 +1,14 @@
 # datetime module was imported into code
 from datetime import datetime
 
-# thus creating a variable to receive the module
-n = datetime.now()
+name = "Kaio"
 
-print("Hello, World!")
-# we use strftime to standardize
-print("Today is", n.strftime("%Y-%m-%d %H:%M:%S"))
+def greeting(name):
+    # thus creating a variable to receive the module
+    n = datetime.now()
+    # we use strftime to standardize
+    print("Hello", name, "today is", n.strftime("%Y-%m-%d %H:%M:%S"))
+
+greeting(name)
+
+
